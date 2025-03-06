eeab6b9cfa83d5f04fda7465c2c431fa6d745ebc
IuriiAv <avramenko.ca@gmail.com>
2025-03-06 12:01:15 +0300

    add social network init

---
+++ src/main/java/org/telran/social/controller/NetworkUserController.java
@@ -0,0 +1,99 @@
+package org.telran.social.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.telran.social.dto.NetworkUserResponseDto;
+import org.telran.social.model.NetworkUser;
+import org.telran.social.service.NetworkUserService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * CRUD - create, read, update ,delete
+ *
+ * @RestController = @Controller + @ResponseBody
+ * @ResponseBody - означает, что можно возвращать объекты из методов контроллера
+ * и они будут преобразованы в нашем случае в JSON формат
+ * <p>
+ * Наше веб приложение доступно по адресу http://localhost:8080/
+ * WEB -> DispatcherServlet (распределяет входящие запросы по контроллерам)
+ * для выбора нужного контроллера исползуем @RequestMapping
+ * После добавления вот этой аннотации @RequestMapping("/api/users")
+ * наш контроллер доступен по адресу :
+ * http://localhost:8080/api/users
+ * <p>
+ * REST API :
+ * GET  http://localhost:8080/api/users - get all users
+ * GET  http://localhost:8080/api/users/12 - get user by id = 12
+ * POST http://localhost:8080/api/users - create user
+ * DELETE http://localhost:8080/api/users/23 - delete user by id = 23
+ * @PathVariable - когда параметр является частью url
+ * <p>
+ * Путь до конкретного меда контроллера складывается из
+ * Имя хоста : localhost
+ * Номер порта : 8080
+ * Адрес контроллера (@RequestMapping) : /api/users
+ * Адрес метода (@GetMapping,@PostMapping etc) :
+ * <p>
+ * JSON - JavaScript object notation (human readable)
+ * XML
+ * <p>
+ * FROM WEB to SERVER -> JSON -> convert to Java Object - jackson lib.
+ * FROM SERVER to WEB -> Java Object -> convert to JSON - jackson lib.
+ * <p>
+ * DTO - data transfer object :
+ * getById -> NetworkUser(id,name,surname,login,password) -> convert to DTO ->
+ * -> NetworkUserResponseDto(id, name, surname) -> convert to JSON -> REST
+ * <p>
+ * JSON(name,surname,login) -> convert to NetworkUserRequestDto(name,surname,login) ->
+ * service -> NetworkUserRequestDto -> NetworkUser
+ */
+
+@RestController
+@RequestMapping("/api/users")
+public class NetworkUserController {
+
+    @Autowired
+    private NetworkUserService userService;
+
+    // GET http://localhost:8080/api/users
+    @GetMapping // - GET запрос
+    public List<NetworkUserResponseDto> getAll() {
+
+        return userService.getAll().stream().map(user -> NetworkUserResponseDto.builder()
+                        .name(user.getName())
+                        .id(user.getId())
+                        .surname(user.getSurname())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    //1. Передача параметра в метод как часть адреса @PathVariable
+    //GET http://localhost:8080/api/users/12
+    @GetMapping("/{id}")
+    public NetworkUser getById(@PathVariable(name = "id") Long id) {
+        return userService.getById(id);
+    }
+
+    //POST http://localhost:8080/api/users
+    //2. Передача параметра в метод в теле запроса @RequestBody
+    @PostMapping // - POST запрос
+    public NetworkUser create(@RequestBody NetworkUser networkUser) {
+        return userService.create(networkUser);
+    }
+
+    //GET http://localhost:8080/api/users/filter?minAge=18&maxAge=35
+    //3. Передача параметра в метод как параметры запроса(через адресную строку) @RequestParam
+    @GetMapping("/filter")
+    public List<NetworkUser> filter(@RequestParam(name = "minAge", required = false) Long minAge,
+                                    @RequestParam(name = "maxAge", required = false) Long maxAge) {
+        return userService.filter(minAge, maxAge);
+    }
+
+    //DELETE http://localhost:8080/api/users/23
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable(name = "id") Long id) {
+        userService.deleteById(id);
+    }
+}
---
+++ src/main/java/org/telran/social/repository/InMemoryNetworkUserRepository.java
@@ -0,0 +1,62 @@
+package org.telran.social.repository;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import org.telran.social.model.NetworkUser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class InMemoryNetworkUserRepository implements NetworkUserRepository {
+
+    private Map<Long, NetworkUser> storage = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        storage.put(1L, NetworkUser
+                .builder()
+                .id(1L)
+                .name("Alex")
+                .surname("Alexeev")
+                .login("alex345")
+                .password("alexalex")
+                .build());
+
+        storage.put(2L, NetworkUser
+                .builder()
+                .id(2L)
+                .name("Oleg")
+                .surname("Olegov")
+                .login("oleg56456")
+                .password("53453454")
+                .build());
+    }
+
+    @Override
+    public List<NetworkUser> getAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+    @Override
+    public NetworkUser getById(Long id) {
+        return storage.get(id);
+    }
+
+    @Override
+    public NetworkUser create(NetworkUser networkUser) {
+        return storage.put(networkUser.getId(), networkUser);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        storage.remove(id);
+    }
+
+    @Override
+    public List<NetworkUser> filter(Long minAge, Long maxAge) {
+        return null;
+    }
+}
